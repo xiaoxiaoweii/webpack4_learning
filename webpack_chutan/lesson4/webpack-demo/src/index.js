cfa743e8f8f607e88a79d5cbd72a68422f2d8bb6
“晓晓伟” <2679656468@qq.com>
2019-11-30 11:51:11 +0800

    使用配置文件

---
+++ webpack_chutan/lesson4/webpack-demo/src/index.js
@@ -0,0 +1,9 @@
+// Es Module 模块引入方式 引入报错  浏览器不认识  webpack中的模块可以解决这个问题
+import Header from './header.js'
+import Content from './content.js'
+import Sidebar from './sidebar.js'
+
+new Header();
+new Sidebar();
+new Content();
+
